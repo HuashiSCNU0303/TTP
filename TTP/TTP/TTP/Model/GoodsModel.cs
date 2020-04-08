57e02b39e68b1f117bbd0c620069434a33aae0b2
PlumSeasonE <781500106@qq.com>
2020-04-08 16:38:15 +0800

    商品详细界面的初步布局

--- TTP/TTP/TTP/Model/GoodsModel.cs
+++ TTP/TTP/TTP/Model/GoodsModel.cs
@@ -4,10 +4,25 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace TTP.Model
-{
+{   //物品类型种类
+    public enum TYPE { AAA, BBB, CCC }
     public class GoodsModel : ViewModelBase
     {
+        
+        
+        //物品ID
         public string Id { get; set; }
-        public string Description { get; set; }
+        //物品名字
+        public string name { get; set; }
+        //物品价格
+        public string price { get; set; }
+        //物品类型
+        public TYPE type { get; set; }
+        //物品拥有者
+        public string owner { get; set; }
+        //物品发布时间
+        public string date { get; set; }
+        //物品的描述，一般由发布者写入
+        public string Description { get; set;}
     }
 }
--- TTP/TTP/TTP/ViewModel/GoodsViewModel.cs
+++ TTP/TTP/TTP/ViewModel/GoodsViewModel.cs
@@ -12,10 +12,10 @@ namespace TTP.ViewModel
         private ObservableCollection<GoodsModel> goodsModels;
         public GoodsViewModel() {
             goodsModels = new ObservableCollection<GoodsModel>();
-            goodsModels.Add(new GoodsModel() { Id = "001", Description="第一个测试商品"});
-            goodsModels.Add(new GoodsModel() { Id = "002", Description="第二个测试商品"});
-            goodsModels.Add(new GoodsModel() { Id = "003", Description ="第三个测试商品"});
-            goodsModels.Add(new GoodsModel() { Id = "004", Description ="第四个测试商品"});
+            goodsModels.Add(new GoodsModel() { Id = "001", name = "书", price = "50", type = TYPE.AAA, owner = "张三", date="2020.1.1", Description = "第一个测试商品" });
+            goodsModels.Add(new GoodsModel() { Id = "002", name = "笔", price = "20", type = TYPE.AAA, owner = "张三", date = "2020.1.2", Description ="第二个测试商品"});
+            goodsModels.Add(new GoodsModel() { Id = "003", name = "斗破苍穹单行本", price = "100", type = TYPE.BBB, owner = "李四", date = "2020.2.1", Description ="第三个测试商品"});
+            goodsModels.Add(new GoodsModel() { Id = "004", name = "充电宝", price = "3000", type = TYPE.CCC, owner = "王五", date = "2020.1.14", Description ="第四个测试商品"});
         }
 
         public ObservableCollection<GoodsModel> GoodsModels {
