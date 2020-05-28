0cc6e204f8d74acd02fcc10fb2c57aded5d4c527
Ye_Junhao <HuashiSCNU0303@hotmail.com>
2020-05-28 16:07:37 +0800

    首页布局小改，数据绑定小改

--- TTP/TTP/TTP/View/AddTimeRecordPage.xaml.cs
+++ TTP/TTP/TTP/View/AddTimeRecordPage.xaml.cs
@@ -13,6 +13,7 @@ using TTP.Data;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Globalization;
 
 namespace TTP.View
 {
@@ -23,9 +24,36 @@ namespace TTP.View
         public event Action<int, int, string> SetTimeEvent;
         int hours_int = 0, minutes_int = 0;
         string description;
+
+        public static readonly BindableProperty HoursProperty = BindableProperty.Create("Hours", typeof(int), typeof(AddTimeRecordPage), 0);
+        public static readonly BindableProperty MinutesProperty = BindableProperty.Create("Minutes", typeof(int), typeof(AddTimeRecordPage), 0);
+        public static readonly BindableProperty DescriptionProperty = BindableProperty.Create("Description", typeof(string), typeof(AddTimeRecordPage), "");
+        public int Hours
+        {
+            set { SetValue(HoursProperty, value); }
+            get { return (int)GetValue(HoursProperty); }
+        }
+        public int Minutes
+        {
+            set { SetValue(MinutesProperty, value); }
+            get { return (int)GetValue(MinutesProperty); }
+        }
+        public string Description
+        {
+            set { SetValue(DescriptionProperty, value); }
+            get { return (string)GetValue(DescriptionProperty); }
+        }
+
         public AddTimeRecordPage()
         {
             InitializeComponent();
+
+            entryHours.BindingContext = this;
+            entryHours.SetBinding(Entry.TextProperty, new Binding("Hours") { Converter = new ExamineConverter(), Mode = BindingMode.OneWayToSource });
+            entryMinutes.BindingContext = this;
+            entryMinutes.SetBinding(Entry.TextProperty, new Binding("Minutes") { Converter = new ExamineConverter(), Mode = BindingMode.OneWayToSource });
+            entryDescription.BindingContext = this;
+            entryDescription.SetBinding(Entry.TextProperty, new Binding("Description"));
         }
 
         private void btnReturn_Clicked(object sender, EventArgs e)
@@ -35,35 +63,49 @@ namespace TTP.View
 
         protected override void OnDisappearing()
         {
-            SetTimeEvent(hours_int, minutes_int, description);
+            if (Hours != 0 || Minutes != 0)
+            {
+                SetTimeEvent(Hours, Minutes, Description);
+            }
             base.OnDisappearing();
         }
 
         private async void btnAddRecord_Clicked(object sender, EventArgs e)
         {
-            string hours = entryHours.Text;
-            string minutes = entryMinutes.Text;
-            description = entryDescription.Text;
-            try
+            if (Hours < 0 || Minutes < 0) 
             {
-                hours_int = Convert.ToInt32(hours);
-                minutes_int = Convert.ToInt32(minutes);
-            }
-            catch (InvalidCastException)
-            {
-                await DisplayAlert("输入有误", "输入不正确，请重新输入！", "OK");
+                await DisplayAlert("输入有误", "请重新输入！", "OK");
                 return;
             }
-            if (minutes_int >= 60)
+            if (Minutes >= 60)
             {
                 await DisplayAlert("输入有误", "分钟数应该＜60，请重新输入！", "OK");
                 return;
             }
-
-            Console.WriteLine("输入时：" + description);
-            await DisplayAlert("添加成功", "开始锁机啦！", "OK");
+            
             await PopupNavigation.Instance.PopAsync();
 
+        }
+    }
+
+    class ExamineConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                int value_int = System.Convert.ToInt32(value);
+            }
+            catch (FormatException)
+            {
+                return -1;
+            }
+            return value;
         }
     }
 }
